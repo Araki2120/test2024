290f4c5c91f39cb4484f42c7101d7ea2240dffa3
Araki2120 <144511024+Araki2120@users.noreply.github.com>
2024-03-04 23:31:40 +0900

    add:GSAPアニメーション練習追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+//GSAPアニメーション
+const mv = document.querySelector('#mv');
+
+//スクロールに応じたもの（勝手に逆再生される）
+gsap.to('.mv__ttl', {
+  translateY: -300,
+  scrollTrigger: {
+    //timelineの設定
+    trigger: mv,
+    //要素の　開始・終了位置　画面に対しての開始・終了ライン
+    start: 'top top',
+    end: '50% top', //終了位置
+    markers: true,
+    pin: true, //画面止まるかどうか
+    scrub: true,//数字にして、増えると余韻が付く
+  }
+});
+
+//スクロールに応じないもの（自動アニメ・逆再生可能）
+// gsap.to('.mv__ttl', {
+//   y: -300,
+//   duration: 2,//所要時間
+//   scrollTrigger: {
+//     toggleActions: 'play none none reverse',
+//     trigger: mv,
+//     //要素の　開始・終了位置　画面に対しての開始・終了ライン
+//     start: 'top top',//開始位置だけでOK
+//     markers: true,
+//     // pin: true, 必要ない！
+//     // scrub: true, 必要ない！
+//   }
+// });
+
 //スライドメニュー
 const openBtn = document.querySelector('.menu__openBtn');
 const closeBtn = document.querySelector('.menu__closeBtn');
@@ -7,52 +40,52 @@ const menuLinks = document.querySelectorAll('.menu__link');
 
 // 開く設定
 openBtn.addEventListener('click', () => {
-    menu.classList.add('openMenu');
+  menu.classList.add('openMenu');
 
-    menuLists.forEach((menuList, i) => {
-        setTimeout(() => {
-            menuList.classList.add('slideList');
-        }, 300 * i);
-    });
+  menuLists.forEach((menuList, i) => {
+    setTimeout(() => {
+      menuList.classList.add('slideList');
+    }, 300 * i);
+  });
 });
 
 // 閉じる設定
 const closeMenu = () => {
-    menu.classList.remove('openMenu');
-    menu.classList.add('closeMenu');
+  menu.classList.remove('openMenu');
+  menu.classList.add('closeMenu');
 
-    menuLists.forEach((menuList, i) => {
-        menuList.classList.remove('slideList');
-    });
+  menuLists.forEach((menuList, i) => {
+    menuList.classList.remove('slideList');
+  });
 };
 
 closeBtn.addEventListener('click', () => {
-    closeMenu();
+  closeMenu();
 })
 
 window.addEventListener('scroll', () => {
-    closeMenu();
+  closeMenu();
 });
 
 menuLinks.forEach((menuLink) => {
-    menuLink.addEventListener('click', (e) => {
-        e.stopPropagation();
-        closeMenu();
-    });
+  menuLink.addEventListener('click', (e) => {
+    e.stopPropagation();
+    closeMenu();
+  });
 });
 
 
 //スクロールに応じたフェードイン
 // 実行する処理
 const animationFade = (entries, obs) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            console.log('ok');
-            entry.target.classList.add('fadeInAnimation');
-            // 終わったら監視の終了
-            obs.unobserve(entry.target);
-        };
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log('ok');
+      entry.target.classList.add('fadeInAnimation');
+      // 終わったら監視の終了
+      obs.unobserve(entry.target);
+    };
+  });
 };
 
 // 監視設定
@@ -62,7 +95,7 @@ const fadeObserver = new IntersectionObserver(animationFade);
 const fadeElements = document.querySelectorAll('.fadeIn');
 
 fadeElements.forEach((fadeElement) => {
-    fadeObserver.observe(fadeElement);
+  fadeObserver.observe(fadeElement);
 });
 
 
@@ -72,10 +105,10 @@ const mainImg = document.querySelector('.gallery__mainImg');
 const tmbImgs = document.querySelectorAll('.gallery__img');
 
 tmbImgs.forEach((tmb) => {
-    tmb.addEventListener('mouseover', (e) => {
-        mainImg.src = e.target.src;
-        mainPic.animate(
-            //単純なアニメーションのためJSで記載
-            { opacity: [0, 1] }, 500);
-    });
+  tmb.addEventListener('mouseover', (e) => {
+    mainImg.src = e.target.src;
+    mainPic.animate(
+      //単純なアニメーションのためJSで記載
+      { opacity: [0, 1] }, 500);
+  });
 });
